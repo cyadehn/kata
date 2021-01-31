95ee8c6808eab6229231abd27dbac3fd0d062b92
Chris Dehner <topherdehner@gmail.com>
2021-01-31 10:30:39 -0500

    Test: Alpha numeric chars return > 0, non-alpha numeric return -1

--- dups/dups.test/GetDupsTest.cs
+++ dups/dups.test/GetDupsTest.cs
@@ -1,14 +1,32 @@
 using System;
+using dups;
 using Xunit;
 
 namespace dups.test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("abcde", 0)]
+        [InlineData("aabbcde", 2)]
+        [InlineData("aabBcde", 2)]
+        [InlineData("indivisibility", 1)]
+        [InlineData("Indivisibilities", 2)]
+        [InlineData("aA11", 2)]
+        [InlineData("ABBA", 2)]
+        public void AlphaNumChars(string test, int expected)
         {
-
+            int target = dups.Util.FindDups(test);   
+            Assert.Equal(target, expected);
+        }
+        [Theory]
+        [InlineData("a##%cde")]
+        [InlineData("abc)e")]
+        [InlineData("abcd^")]
+        public void NonAlphaNumChars(string test)
+        {
+            int target = dups.Util.FindDups(test);
+            Assert.Equal(target, -1);
         }
     }
 }
