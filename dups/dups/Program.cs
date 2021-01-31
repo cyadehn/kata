0a01f4c3687e13b9ce7177a2d7a486d29deca12d
Chris Dehner <topherdehner@gmail.com>
2021-01-31 11:06:37 -0500

    Check for non-alpha or non-numeric characters

--- dups/dups/Program.cs
+++ dups/dups/Program.cs
@@ -6,7 +6,7 @@ namespace dups
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int result = Util.FindDups("#aabCe");
         }
     }
     public static class Util
@@ -14,8 +14,36 @@ namespace dups
         public static int FindDups(string input)
         {
             int dupNum = 0;
-            
+            // Convert string to array of characters + sort
+            char[] chars = input.ToCharArray();
+            Array.Sort(chars);
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                char current = char.ToUpper(chars[i]);
+                // If current char is non-alpha or non-numeric, return -1 to allow program to re-prompt user for input
+                if (!IsAlphaNum(current))
+                {
+                    return -1;
+                }
+            }
             return dupNum;
+        }
+        private static bool IsAlphaNum(char input)
+        {
+            char c = char.ToUpper(input);
+            if (c >= '0' && c <= '9')
+            {
+                return true;
+            }
+            else if (c >= 'A' && c <= 'Z')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
